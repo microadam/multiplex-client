1f26dc391ae93881d11ad4fe35659b60f2c7d26d
Simon Adams <simonadams@gmail.com>
2016-11-01 14:17:06 +0000

    added reconnection, resumption

--- client.js
+++ client.js
@@ -2,18 +2,34 @@ var client = require('socket.io-client');
 var minimist = require('minimist');
 
 var argv = minimist(process.argv.slice(2), { default: {user: 'admin', pass: 'admin'} });
-var queryId = 0;
+var queryId = 0,
+    sessionId,
+    queries = {};
 
 var queryResponse = function(data, cb, requestId) {
     console.log('Query '+requestId+'\t: '+data.length+' items');
+    console.log(data);
     cb();
 }
 
 var submitQuery = function(socket, connectionId, resource, params) {
     var newQueryId = ++queryId;
     socket.emit('query', { type: resource, requestId: newQueryId, params: params });
+    console.log('Starting Query '+newQueryId);
+    queries[newQueryId] = connectionId+newQueryId; 
     socket.on(connectionId+newQueryId, function(data, cb) {
         queryResponse(data, cb, newQueryId);
+    });
+}
+
+var stopQuery = function() {
+    
+}
+
+var resumeQuery = function(socket, queryId, resumeId) {
+    console.log('Will resume query '+queryId+': '+resumeId);
+    socket.emit('resumeQuery', {
+        id: resumeId 
     });
 }
 
@@ -23,18 +39,46 @@ if (!argv['host']) {
 } else {
     console.log('Connecting to '+argv['host']);
     var socket = client(argv['host'], {
-        reconnectionDelay: 5000,
         rejectUnauthorized: false,
         transports: [ 'websocket' ]
     });
 
     socket.on('connect', function() {
         console.log('Connected');
-        console.log('Authenticating using user/pass');
-        socket.emit('authentication', {
-            username: argv['user'],
-            password: argv['pass']
-        });
+        if (sessionId) {
+            console.log('Authenticating using session '+sessionId);
+            socket.emit('authentication', {
+                sessionId: sessionId
+            });       
+        } else {
+            console.log('Authenticating using user/pass');
+            socket.emit('authentication', {
+                username: argv['user'],
+                password: argv['pass']
+            });
+        }
+    });
+
+    socket.on('unauthorized', function(err) {
+        console.log('Authorization failure');
+        console.log('\t'+err.message);
+    });
+
+    socket.on('error', function(err) {
+        console.log('Error');
+        console.log(err);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('Disconnected');
+    });
+
+    socket.on('reconnecting', function (number) {
+        console.log('Reconnecting (attempt ' + number + ')');
+    });
+
+    socket.on('reconnect_error', function (err) {
+        console.log('Couldn\'t reconnect');
     });
 
     socket.on('reconnect', function() {
@@ -45,8 +89,18 @@ if (!argv['host']) {
         console.log('Authentication successful');
         console.log('\tConnection: '+session.connectionId);
         console.log('\tSession: '+session.sessionId);
-        submitQuery(socket,session.connectionId, 'event', { element_id: [9010]});
-        submitQuery(socket,session.connectionId, 'alarm', { element_id: [9010]});
+        sessionId = session.sessionId;
+        var queryKeys = Object.keys(queries);
+        if (queryKeys.length) {
+            queryKeys.forEach(function(key) {
+                resumeQuery(socket, key, queries[key]);
+            });
+        } else {
+            //submitQuery(socket,session.connectionId, 'event', { element_id: [9010]});
+            //submitQuery(socket,session.connectionId, 'alarm', { element_id: [9010]});
+            submitQuery(socket,session.connectionId, 'stat', { element_id: [6583], metric_id: [1316], resolution: 30 });
+            
+        }
     });
 
     socket.on('queryError', function(err) {
@@ -54,9 +108,15 @@ if (!argv['host']) {
         console.log(err);
     });
 
-    socket.on('unauthorized', function(err) {
-        console.log('Authorization failure');
-        console.log('\t'+err.message);
+    socket.on('queryEnd', function(data) {
+        console.log('Query End');
+        console.log(data);
     });
+    
+    socket.on('queryInfo', function(info) {
+        console.log('Query info');
+        console.log(info);
+    });
+
 
 }
